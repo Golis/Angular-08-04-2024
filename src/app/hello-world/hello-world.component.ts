783b65f48485afdcbbdb35ede6e845cab0df71c6
Diego Anton Cerqueira <Diego.Anton.Cerqueira@es.ey.com>
2024-04-08 22:19:54 +0200

    first commit

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css'
+})
+export class HelloWorldComponent {
+  name:string ="Diego";
+
+  onButtonClick(){
+    this.name="Antón";
+  }
+}
